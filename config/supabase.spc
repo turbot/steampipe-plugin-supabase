9a87d6751e7abcf9f146bdf4a6f64d96e77782b5
Subhajit Mondal <subhajit@turbot.com>
2023-02-14 17:24:57 +0530

    Initial commit

---
+++ config/supabase.spc
@@ -0,0 +1,8 @@
+connection "supabase" {
+  plugin = "supabase"
+
+  # The Supabase personal token.
+  # All API requests require a Supabase Personal token to be included in the Authorization header.
+  # To generate or manage your API token, visit your account page: https://app.supabase.com/account/tokens
+  # access_token = "sbp_bdd0xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx4f23"
+}
---
+++ supabase/plugin.go
@@ -0,0 +1,26 @@
+package supabase
+
+import (
+	"context"
+
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
+)
+
+const pluginName = "steampipe-plugin-supabase"
+
+// Plugin creates this (supabase) plugin
+func Plugin(ctx context.Context) *plugin.Plugin {
+	p := &plugin.Plugin{
+		Name:             pluginName,
+		DefaultTransform: transform.FromCamel().Transform(transform.NullIfZeroValue),
+		DefaultGetConfig: &plugin.GetConfig{},
+		ConnectionConfigSchema: &plugin.ConnectionConfigSchema{
+			NewInstance: ConfigInstance,
+			Schema:      ConfigSchema,
+		},
+		TableMap: map[string]*plugin.Table{},
+	}
+
+	return p
+}
