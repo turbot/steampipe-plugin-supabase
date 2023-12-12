a2358482ada15e0d2b22cdce55cb23b68fb64ef5
misraved <ved@turbot.com>
2023-12-12 16:02:42 +0530

    Update connection config variable definition to use the latest hcl syntax

--- supabase/plugin.go
+++ supabase/plugin.go
@@ -17,7 +17,6 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 		DefaultGetConfig: &plugin.GetConfig{},
 		ConnectionConfigSchema: &plugin.ConnectionConfigSchema{
 			NewInstance: ConfigInstance,
-			Schema:      ConfigSchema,
 		},
 		TableMap: map[string]*plugin.Table{
 			"supabase_function":                    tableSupabaseFunction(ctx),
